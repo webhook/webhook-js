a649e0cb4a923f86b14d18a9ebe72c10b6aef247
gpbmike <gpbmike@gmail.com>
2013-07-02 10:38:31 -0700

    Local modal support.

--- js/webhook-modal.js
+++ js/webhook-modal.js
@@ -16,12 +16,51 @@
 
   Modal.prototype = {
     init: function (element, options) {
-      return [element, options];
+      this.$element = $(element);
+      this.options  = this.getOptions(options);
+      this.$body    = $('body');
+    },
+
+    getOptions: function (options) {
+      return $.extend({}, $.fn.modal.defaults, this.$element.data(), options);
+    },
+
+    getTarget: function () {
+
+      var selector;
+
+      selector = this.$element.attr('href');
+      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '');
+
+      return this.$target = $(selector);
+
+    },
+
+    getModal: function () {
+      this.$modal = this.$modal || $(this.options.template);
+      this.$modal.one('click.modal', function (event) { event.stopPropagation(); });
+      return this.$modal;
+    },
+
+    getScreen: function () {
+      this.$screen = this.$screen || $(this.options.screentemplate);
+      this.$screen.one('click.modal', $.proxy(this.hide, this));
+      return this.$screen;
+    },
+
+    show: function () {
+      this.$body.append(this.getScreen(), this.getModal().append(this.getTarget()));
+    },
+
+    hide: function () {
+      this.getScreen().detach();
+      this.getModal().detach();
     }
   };
 
-  /* MODAL PLUGIN DEFINITION
-   * ======================= */
+
+ /* MODAL PLUGIN DEFINITION
+  * ======================= */
 
   $.fn.modal = function (option) {
     return this.each(function () {
@@ -41,6 +80,19 @@
 
   $.fn.modal.Constructor = Modal;
 
-  $.fn.modal.defaults = {};
+  $.fn.modal.defaults = {
+    cache: true,
+    template: "<div class='wh-modal'></div>",
+    screentemplate: "<div class='wh-modal-screen'></div>"
+  };
+
+
+ /* MODAL DATA API
+  * ============== */
+
+  $(document).on('click.modal.data-api', '[data-toggle=modal]', function (e) {
+    e.preventDefault();
+    $(this).modal('show');
+  });
 
 }(window.jQuery));
